f4b853f1c496175f63fa3b0c2d1a55f2d8c2e060
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-03-02 00:56:40 +0100

    Tidy up code for division game

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -149,10 +149,9 @@ function displayDivisionQuestion(operand1, operand2) {
 
     operand1 = operand1 * operand2;
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "/";
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
 
 
